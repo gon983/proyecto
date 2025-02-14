09c6c20eed6ddbb18bfb857363dae1d6173e3aa4
Gonzalo Dutari <gonzalo_dutari@outlook.com>
2025-02-14 19:44:38 -0300

    product state

--- mvp/src/main/java/com/proyect/mvp/domain/model/entities/ProductStateEntity.java
+++ mvp/src/main/java/com/proyect/mvp/domain/model/entities/ProductStateEntity.java
@@ -1,21 +1,31 @@
 package com.proyect.mvp.domain.model.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
+import java.util.UUID;
 
-
-@Table( "productstate")
+@Builder
+@AllArgsConstructor
+@Table("productstate")
 @Getter
 @NoArgsConstructor
 public class ProductStateEntity {
 
     @Id
-    
-    @Column( "id_product_state")
-    private String idProductState;
+    @Column("id_product_state")
+    private UUID idProductState;
 
-    @Column( "name")
+    @Column("name")
     private String name;
+
+    public ProductStateEntity(String name) {
+        this.idProductState = UUID.randomUUID();
+        this.name = name;
+    }
 }
--- mvp/src/main/java/com/proyect/mvp/domain/repository/ProductStateRepository.java
+++ mvp/src/main/java/com/proyect/mvp/domain/repository/ProductStateRepository.java
@@ -1,9 +1,14 @@
 package com.proyect.mvp.domain.repository;
 
-import org.springframework.data.r2dbc.repository.R2dbcRepository;
-
 import com.proyect.mvp.domain.model.entities.ProductStateEntity;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Mono;
 
-public interface ProductStateRepository extends R2dbcRepository<ProductStateEntity,String> {
+import java.util.UUID;
 
+public interface ProductStateRepository extends R2dbcRepository<ProductStateEntity, UUID> {
+
+    @Query("INSERT INTO productstate (id_product_state, name) VALUES (:idProductState, :name)")
+    Mono<ProductStateEntity> insertProductState(UUID idProductState, String name);
 }
