a801087265e4efdf457eabcde6aefeebdb1b42d6
Gonzalo Dutari <gonzalo_dutari@outlook.com>
2025-02-17 20:15:54 -0300

    deletes jeje

--- mvp/src/main/java/com/proyect/mvp/domain/repository/ProductCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyect.mvp.domain.repository;
-
-import org.springframework.data.r2dbc.repository.R2dbcRepository;
-
-import com.proyect.mvp.domain.model.entities.ProductCategoryEntity;
-
-public interface ProductCategoryRepository extends R2dbcRepository<ProductCategoryEntity,String> {
-
-}
