bd958322b6b226fdb95d6508624dd832795d979c
Gonzalo Dutari <gonzalo_dutari@outlook.com>
2025-04-18 20:42:57 -0300

    pareciera q va a funcionar el chat

--- mvp/src/main/java/com/proyect/mvp/infrastructure/routes/ChatRouter.java
+++ mvp/src/main/java/com/proyect/mvp/infrastructure/routes/ChatRouter.java
@@ -18,6 +18,7 @@ import com.proyect.mvp.application.services.ChatMessageService;
 import com.proyect.mvp.application.services.DeviceTokenService;
 import com.proyect.mvp.domain.model.entities.ChatMessageEntity;
 import com.proyect.mvp.domain.model.entities.DeviceTokenEntity;
+import com.proyect.mvp.infrastructure.security.UserContextService;
 
 import reactor.core.publisher.Mono;
 
@@ -25,18 +26,19 @@ import reactor.core.publisher.Mono;
 public class ChatRouter {
     
     @Bean
-    public RouterFunction<ServerResponse> chatRoutes(ChatMessageService chatMessageService, DeviceTokenService deviceTokenService) {
-        return route(GET("/api/chat/messages/{userId}"), request -> getUserMessages(request, chatMessageService))
+    public RouterFunction<ServerResponse> chatRoutes(ChatMessageService chatMessageService, DeviceTokenService deviceTokenService, UserContextService userContext) {
+        return route(GET("/api/chat/messages"), request -> getUserMessages(request, chatMessageService, userContext))
                 .andRoute(POST("/api/chat/user-message"), request -> createUserMessage(request, chatMessageService))
                 .andRoute(POST("/api/chat/company-message"), request -> createCompanyMessage(request, chatMessageService))
                 .andRoute(PUT("/api/chat/read/{messageId}"), request -> markAsRead(request, chatMessageService))
                 .andRoute(POST("/api/chat/register-token"), request -> registerDeviceToken(request, deviceTokenService))
-                .andRoute(DELETE("/api/chat/unregister-token/{userId}/{deviceType}"), request -> unregisterDeviceToken(request, deviceTokenService));
+                .andRoute(DELETE("/api/chat/unregister-token/{userId}/{deviceType}"), request -> unregisterDeviceToken(request, deviceTokenService, userContext));
     }
 
-    private Mono<ServerResponse> getUserMessages(ServerRequest request, ChatMessageService chatMessageService) {
-        UUID userId = UUID.fromString(request.pathVariable("userId"));
-        return ServerResponse.ok().body(chatMessageService.getUserMessages(userId), ChatMessageEntity.class);
+    private Mono<ServerResponse> getUserMessages(ServerRequest request, ChatMessageService chatMessageService, UserContextService userContext) {
+        
+        return userContext.getCurrentIdUser().flatMap(userId ->        
+        ServerResponse.ok().body(chatMessageService.getUserMessages(UUID.fromString(userId)), ChatMessageEntity.class));
     }
 
     private Mono<ServerResponse> createUserMessage(ServerRequest request, ChatMessageService chatMessageService) {
@@ -63,10 +65,13 @@ public class ChatRouter {
                 .flatMap(savedToken -> ServerResponse.ok().bodyValue(savedToken));
     }
 
-    private Mono<ServerResponse> unregisterDeviceToken(ServerRequest request, DeviceTokenService deviceTokenService) {
-        UUID userId = UUID.fromString(request.pathVariable("userId"));
+    private Mono<ServerResponse> unregisterDeviceToken(ServerRequest request, DeviceTokenService deviceTokenService,UserContextService userContext) {
+        
         String deviceType = request.pathVariable("deviceType");
-        return deviceTokenService.deleteDeviceToken(userId, deviceType)
-                .then(ServerResponse.ok().build());
+        
+        
+        return userContext.getCurrentIdUser().flatMap(userId ->  
+        deviceTokenService.deleteDeviceToken(UUID.fromString(userId), deviceType)
+                .then(ServerResponse.ok().build()));
     }
 }
